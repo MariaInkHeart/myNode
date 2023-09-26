c364a9cfcba653cc31c6452dc3c1d1003f1c599e
Maria Serditova <mariaserditova@mail.ru>
2023-09-26 20:47:55 +0300

    main component and routing

--- Desktop/курсач блог/my-blog/src/app.js
+++ Desktop/курсач блог/my-blog/src/app.js
@@ -1,9 +1,55 @@
+import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+
+const Content = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => <div>Hat</div>;
+const Footer = () => <div>Foot</div>;
 
 export const App = () => {
 	return (
-		<div className={styled.app}>
-			<div></div>
-		</div>
+		<>
+			<Header />
+			<Content>
+				<H2>HELLO WORLD</H2>
+				<Routes>
+					<Route
+						path="/"
+						element={<div>main</div>}
+					/>
+					<Route
+						path="/login"
+						element={<div>Log in</div>}
+					/>
+					<Route
+						path="/register"
+						element={<div>Register</div>}
+					/>
+					<Route
+						path="/users"
+						element={<div>Users</div>}
+					/>
+					<Route
+						path="/post"
+						element={<div>New article</div>}
+					/>
+					<Route
+						path="/post/:postId"
+						element={<div>Article</div>}
+					/>
+					<Route
+						path="*"
+						element={<div>Error</div>}
+					/>
+				</Routes>
+			</Content>
+			<Footer />
+		</>
 	);
 };
